dfb08ec4e3f6eb9d21fcb65c0bfcc1a91890e081
Charles Ballew <charleswballewjr@gmail.com>
2018-01-18 22:51:26 -0700

    restart starter files

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const storeController = require("../controllers/storeController");
 
 // Do work here
-// router.get('/', (req, res) => {
-//   res.send('Hey! It works!');
-// });
-
-router.get("/", storeController.homePage);
+router.get('/', (req, res) => {
+  res.send('Hey! It works!');
+});
 
 module.exports = router;
